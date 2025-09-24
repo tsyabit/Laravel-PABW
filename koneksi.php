39e6a01c64d4d26dcdf4ffba8006dae4c8d0d1e9
tsyabit <tsyabitmughni@gmail.com>
2025-09-24 09:37:19 +0700

    hapus file contoh.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "config.php";
-
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
-?>
